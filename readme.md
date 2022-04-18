c346ecc36f113d5e1ed7139f296094e4049aec94
Mart994 <38144617+Mart994@users.noreply.github.com>
2022-04-18 15:32:38 -0300

    Actualizado y personalizado
    Se ha actualizado el script y adaptado con nuevas opciones para ubuntu 22.04

--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
 # Remove snap from Ubuntu
 
-This simple bash script removes snap from Ubuntu. Why? Because snap is severely broken and it completely messes up my servers. The content for this script came from an [Ask Ubuntu answer from Lorenz Keel](https://askubuntu.com/a/1285102/392607). I've put it in github so that I can invoke it on my aws ec2 ubuntu instances as part of my packer builds without having to copy the contents of the script everywhere and eventually lose it.
+Este script remueve snap de Ubuntu 22.04, agrega Flatpak y Flathub, se opta instalar Firefox desde PPA de [Mozilla Team](https://launchpad.net/~mozillateam/+archive/ubuntu/ppa) y se instala la tienda de Gnome Software
 
 **Usage**
 
 ```bash
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/BryanDollery/remove-snap/main/remove-snap.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Mart994/quitar-snap/main/remove-snap.sh)"
 ```
--- remove-snap.sh
+++ remove-snap.sh
@@ -1,16 +1,46 @@
-#!/bin/bash
+#!/bin/bash -e
 
-echo "Removing snap..."
+# Syntax Derived from script Copyright (C) 2019 madaidan under GPL
+# Snap disable forked from https://github.com/BryanDollery
+# 2022 script panzerlop under GPLv3
+
+set -eu -o pipefail # fail on error and report it, debug all lines
+
+
+script_checks() {
+
+ sudo apt-get update
+ 
+echo ""
+    if [[ "$(id -u)" -ne 0 ]]; then
+      echo "Seamos SuperAdmins primero (sudo)."
+      exit 1
+    fi
+}
+
+
+disable_snap() {
+
+
+  read -r -p "¿Desabilitar y quitar snap del sistema? (y/n) " disable_snap
+	  if [ "${disable_snap}" = "y" ]; then
+
+echo "Deteniendo daemon..."
 
 # Stop the daemon
 sudo systemctl stop snapd && sudo systemctl disable snapd
 
+echo "Desinstalando snapd..."
 # Uninstall
 sudo apt purge -y snapd
+sudo apt-mark hold snapd
 
+echo "Limpiando mugre (carpetas de snap)..."
 # Tidy up dirs
-sudo rm -rf /snap /var/snap /var/lib/snapd /var/cache/snapd /usr/lib/snapd ~/snap
+rm -rf ~/snap
+sudo rm -rf /snap /var/snap /var/lib/snapd /var/cache/snapd /usr/lib/snapd
 
+echo "Ahora evitaré la reinstalación de snaps futuros..."
 # Stop it from being reinstalled by 'mistake' when installing other packages
 cat << EOF > no-snap.pref
 Package: snapd
@@ -21,5 +51,138 @@ EOF
 sudo mv no-snap.pref /etc/apt/preferences.d/
 sudo chown root:root /etc/apt/preferences.d/no-snap.pref
 
-# done
-echo "Snap removed"
+    
+  fi
+}
+
+install_flatpak() {
+
+  read -r -p "¿Instalo Flatpak y agrego el repositorio de Flathub? (y/n) " install_flatpak
+	  if [ "${install_flatpak}" = "y" ]; then
+
+        sudo apt update
+      
+        sudo apt install flatpak -y
+
+        sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    fi
+}
+
+install_firefox() {
+echo "Existe la posibilidad de instalar Firefox de dos maneras "
+echo " "
+echo "1- Como flatpak desde Flathub "
+echo "2- Desde repositorio PPA de Mozilla Team - https://launchpad.net/~mozillateam/+archive/ubuntu/ppa"
+echo " "
+read -r -p "¿Desde donde desea hacer la instalación? (1/2/n - No instalar Firefox) " option
+    if ["${option}" = "1"]; then
+        install_firefox_flatpak
+    elif ["${option}" = "2"]; then
+        install_firefox_ppa
+    else
+        echo "No se instalará firefox"
+    fi
+}
+
+install_firefox_flatpak() {
+
+  flatpak install flathub org.mozilla.firefox
+
+echo " To run Firefox from Terminal type the following: "
+echo " "
+echo " flatpak run org.mozilla.firefox "
+echo " "
+echo " Restart session if Firefox doesn't appear in Applications menu "
+echo " "
+
+}
+
+install_firefox_ppa() {
+echo "Agregando repositorio PPA"
+
+    sudo add-apt-repository ppa:mozillateam/ppa -y
+    sudo apt update
+echo "instalando Firefox desde PPA"
+    sleep 2
+    sudo apt install -t 'o=LP-PPA-mozillateam' firefox
+
+#evitar que se instale nuevamente snap al momento de instalar o actualizar firefox
+cat << EOF > mozillateamppa.pref
+Package: firefox*
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 501
+EOF
+
+sudo mv no-snap.pref /etc/apt/preferences.d/
+sudo chown root:root /etc/apt/preferences.d/mozillateamppa.pref
+}
+
+#crear opcion para instalar la tienda de gnome
+install_gnome_Software() {
+echo "Instalando Gnome Software"
+sudo apt install -y gnome-software gnome-software-plugin-flatpak
+read -r -p "Instalo Synaptic?? (y/n) " r
+  if [ "${r}" = "y" ]; then
+    sudo apt install -y synaptic
+    
+  fi
+}
+
+ending() {
+  ## Reboot
+  echo ""
+   echo " https://github.com/Mart994/quitar-snap"
+   echo ""
+   echo "Hemos terminado"
+    echo ""
+       echo ""
+  read -r -p "Es recomendable reiniciar el sistema, ¿reiniciar ahora? (y/n) " reboot
+  if [ "${reboot}" = "y" ]; then
+    reboot
+    
+  fi
+}
+
+echo ""
+echo "22.04 Disable Snap & Install Flatpak & Firefox Script"
+echo ""
+cat << "EOF"
+               .-.
+         .-'``(|||)
+      ,`\ \    `-`.
+     /   \ '``-.   `
+   .-.  ,       `___:
+  (:::) :        ___
+   `-`  `       ,   :
+     \   / ,..-`   ,
+      `./ /    .-.`
+         `-..-(   ) 
+               `-`
+EOF
+
+echo ""
+echo ""
+echo "Usando este script aceptas que cualquier malfuncionamiento/ daño/ blabla/ en el sistema es tu responsabilidad"
+# "By using this script you accept any break in system function/ damage / blabla is your own fault..."
+echo "Si estas deacuero.."
+# "So if you also agree with that you may..."
+echo ""
+read -r -p "...iniciar script? (y/n) " start
+if [ "${start}" = "n" ]; then
+
+echo ""
+
+  exit 1
+elif ! [ "${start}" = "y" ]; then
+  echo ""
+  echo "You did not enter a correct character."
+  echo ""
+  echo "Be careful when reading through this script..."
+  exit 1
+fi
+
+disable_snap
+install_flatpak
+install_firefox
+install_gnome_Software
+ending 
